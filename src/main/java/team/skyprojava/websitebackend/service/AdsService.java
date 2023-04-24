e9b156d4c5589e6a44266f33b9f56c29d400ac27
Vyacheslav <viacheslav87klerik@gmail.com>
2023-04-24 22:12:26 +0300

    GET запросы, исправлены ошибки в AdsController и т.д.

--- src/main/java/team/skyprojava/websitebackend/service/AdsService.java
+++ src/main/java/team/skyprojava/websitebackend/service/AdsService.java
@@ -1,14 +1,20 @@
 package team.skyprojava.websitebackend.service;
 
+import org.springframework.security.core.Authentication;
 import team.skyprojava.websitebackend.dto.AdsDto;
+import team.skyprojava.websitebackend.dto.CreateAdsDto;
+import team.skyprojava.websitebackend.dto.ResponseWrapperAdsDto;
 import team.skyprojava.websitebackend.entity.Ads;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface AdsService {
 
     Ads getAds(long id);
 
-    List<AdsDto> getAllAds();
+    ResponseWrapperAdsDto getAllAds();
+
+    AdsDto createAds(CreateAdsDto createAdsDto, Authentication authentication) throws IOException;
 
 }
