da516100ce97aeba20143afd6b7771dabbad5287
AllaKulchitskaya <alla070@list.ru>
2023-04-25 11:49:10 +0300

    Подправлены названия классов

--- src/main/java/team/skyprojava/websitebackend/repository/CommentRepository.java
+++ src/main/java/team/skyprojava/websitebackend/repository/CommentRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 
-public interface AdsCommentRepository extends JpaRepository<Comment, Integer> {
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
     List<Comment> findAllByAdsId(Integer adsId);
 
--- src/main/java/team/skyprojava/websitebackend/service/impl/CommentServiceImpl.java
+++ src/main/java/team/skyprojava/websitebackend/service/impl/CommentServiceImpl.java
@@ -2,22 +2,19 @@ package team.skyprojava.websitebackend.service.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import team.skyprojava.websitebackend.dto.CommentDto;
 import team.skyprojava.websitebackend.dto.ResponseWrapperCommentDto;
 import team.skyprojava.websitebackend.mapper.CommentMapper;
-import team.skyprojava.websitebackend.repository.AdsCommentRepository;
+import team.skyprojava.websitebackend.repository.CommentRepository;
 import team.skyprojava.websitebackend.service.CommentService;
-
-import java.util.List;
 
 public class CommentServiceImpl implements CommentService {
     Logger logger = LoggerFactory.getLogger(CommentService.class);
-    AdsCommentRepository adsCommentRepository;
+    CommentRepository commentRepository;
     CommentMapper commentMapper;
     @Override
     public ResponseWrapperCommentDto getAdsComments(int id) {
         logger.info("Was invoked method for get ads comment by id");
-        return commentMapper.toDto(adsCommentRepository.findAllByAdsId(id));
+        return commentMapper.toDto(commentRepository.findAllByAdsId(id));
     }
 
 
