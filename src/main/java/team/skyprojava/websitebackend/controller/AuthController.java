741c5ed34275420528c0a00ea2d6028e30494e85
ANDRE <klai1@mail.ru>
2024-04-18 12:38:10 +0400

    Доработал Controller

---
+++ src/main/java/team/skyprojava/websitebackend/controller/AuthController.java
@@ -0,0 +1,49 @@
+package team.skyprojava.websitebackend.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import team.skyprojava.websitebackend.dto.LoginReqDto;
+import team.skyprojava.websitebackend.dto.RegisterReqDto;
+import team.skyprojava.websitebackend.dto.Role;
+import team.skyprojava.websitebackend.service.AuthService;
+
+import static team.skyprojava.websitebackend.dto.Role.USER;
+
+@Slf4j
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody LoginReqDto req) {
+        if (authService.login(req.getUsername(), req.getPassword())) {
+            System.out.println("Проверка login");
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody RegisterReqDto req) {
+        Role role = req.getRole() == null ? USER : req.getRole();
+        if (authService.register(req, role)) {
+            System.out.println("Проверка register");
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+}
